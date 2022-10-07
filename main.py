8bfadeeaeb854cd4ddf419c4cf5114642fa18ae1
VoltanFire <voltan.fire@orange.fr>
2022-10-07 19:12:40 +0100

    cleanup

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ registeredUser_L = []
 with open(uncrypted, "r") as ofp:
     for line in ofp:
         user,passwd = line.split("@")
-        passwd = passwd.replace("\n", "")
-        database[user] = passwd
-        registeredUser_L.append(user)
+        database[user] = passwd.replace("\n", "")
+        userList.append(user)
 
 crypt(uncrypted,crypted,key)
 
 
 registeredUser = "Registered users |"
-for user in registeredUser_L :
+for user in userList :
      registeredUser += user+"|"
 
+#^^^^^^^^^^^^^^^^^^^am satisfied until there^^^^^^^^^^^^^^^^^^^
 
 while True:
     print(f"\n{registeredUser}")
@@ -47,14 +47,14 @@ while True:
 del database
 print(f"Logged in as {currentUser}")
 
-#--------------------------CODE----------------------------
-
-#Creatinga a new user
+#-------------------------Main code----------------------------
+#TODO trying object oriented commands for a better mainloop
+#Creating a new user
 def createuser():
     print(registeredUser)
     newUser = input("New username > ")
     
-    while newUser in registeredUser_L :
+    while newUser in userList :
         print("Username already taken")
         newUser = input("new username > ")
     
@@ -69,54 +69,51 @@ def createuser():
 
 
 
-
-
-
-#TODO Filemanagment
-
+#TODO Filesystem, filemanagment
 
 
 def admin():
+    #allows you to see the decrypted file to lookout for problems
     if currentUser == "admin":
         crypt(crypted,uncrypted,key)
         a = input("you admined >")
         a += ""
         crypt(uncrypted,crypted,key)
     else :
-        print("you know Y")
+        print("You don't have the permissions to run this command")
 
 def logout():
-    print("you succesfully logged out as " + currentUser)
-    sys.exit()
+    print("\nyou succesfully logged out as " + currentUser)
+    time.sleep(.75)
+    sys.exit(0)
 
 tmp = time.strftime("%d %B %Y %H:%M:%S")
 print(tmp + " > Version fully fonctinnal")
 
 
-
-#mainloop
-
+#fill this as commands get added
 commands= ["logout","admin","create user","help"]
+
 def i_need_help_because_i_may_or_may_not_suck_help_me_step_uku_i_am_stuck_that_is_why_i_am_here():
     print("here are the currently active commands :\n|",end="")
     for e in commands :
         print(e,end="|")
     print("")
 
-print("\nCommand prompt")
-while True:
-    command = input(">")
-    if command not in commands:
-        print("Unkown command")
-    elif command == "admin":
-        admin()
-    elif command == "help":
-        i_need_help_because_i_may_or_may_not_suck_help_me_step_uku_i_am_stuck_that_is_why_i_am_here()
-    elif command == "create user":
-        createuser()
-    elif command == "logout":
-        logout()
-    else :
-        time.sleep(.25)
-        pass
 
+#mainloop (needs to improve this)
+print("\nCommand prompt")
+
+while True:
+    command = input("\main>")
+    match command:
+        case "admin" | "a":
+            admin()
+        case "penis" | "p":
+            print("heheheha panis")
+        case "create user":
+            createuser()
+        case "help":
+            i_need_help_because_i_may_or_may_not_suck_help_me_step_uku_i_am_stuck_that_is_why_i_am_here()
+        case "logout":
+            logout()
