3bcc35c72ff5a0c2e70ffb85bab4e0c0e3788d07
uku <uku3lig@gmail.com>
2022-10-09 14:11:13 +0200

    cleanup database.py

--- database.py
+++ database.py
@@ -1,24 +1,29 @@
 import json
 from pathlib import Path
 
+DATABASE_FILE = "database.json"
+
+
 class Database(dict):
     def __init__(self, jsonn):
         super().__init__(jsonn)
 
     def save(self):
-        with open("database.json", "w+") as db_file:
+        with open(DATABASE_FILE, "w+") as db_file:
             json.dump(self, db_file, indent=2)
 
-_DEFAULT_DATABASE = Database({
+
+DEFAULT_DATABASE = Database({
     "admin": {
         "password": "4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2"
-    } # default password is 'root'
+    }  # default password is 'root'
 })
 
-def get_database() -> Database:
-    if not Path("database.json").is_file(): # Create file if not already on da computer
-        _DEFAULT_DATABASE.save()
-        return _DEFAULT_DATABASE
 
-    with open("database.json", "r") as db_file:
+def get_database() -> Database:
+    if not Path(DATABASE_FILE).is_file():  # Create file if not already on da computer
+        DEFAULT_DATABASE.save()
+        return DEFAULT_DATABASE
+
+    with open(DATABASE_FILE, "r") as db_file:
         return Database(json.load(db_file))
