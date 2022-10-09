1352c659c3fb46b80154539c60324073ff65eb1d
uku <uku3lig@gmail.com>
2022-10-09 14:14:21 +0200

    fix login system to be back after logging out
    this commit also adds a shutdown command

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 # uku seal of approval™ :D
-import hashlib
-import getpass
 
+from getpass import getpass
+from hashlib import sha256
+
+import proie
 from commands import Command
 from database import get_database
 
-while True:
-    currentUser = input("Username: ")
+
+def login() -> str:
     database = get_database()
-    if currentUser in database.keys():
-        testPasswd = getpass.getpass(f"Password for {currentUser}: ")
-        hashTestPasswd = hashlib.sha256(testPasswd.encode("utf8")).hexdigest()
+    while True:
+        while (user_input := input("Username: ")) not in database.keys():
+            print("Invalid username.\n")
 
-        if hashTestPasswd == database[currentUser]["password"]:
-            break
-        else:
+        password_input = sha256(getpass(f"Password for {user_input}: ").encode("utf8")).hexdigest()
+        if password_input != database[user_input]["password"]:
             print("Incorrect password.\n")
-    else:
-        print("Invalid username.\n")
+        else:
+            print(f"Logged in as '{user_input}'.")
+            return user_input
 
-print(f"Logged in as '{currentUser}'.")
 
-#main loop
+# main loop
 while True:
-    cmd = input(f"/main/{currentUser} > ").split()
+    if proie.current_user is None:
+        proie.current_user = login()
+
+    cmd = input(f"/main/{proie.current_user} > ").split()
     if len(cmd) == 0:
         continue
 
     matches = [c for c in Command.__subclasses__() if c().name == cmd[0]]
     if len(matches) == 0:
+        # TODO show actual help instead of this LOL
         print("No command found? :nobitches: heheheha your mom")
     else:
         matches[0]().execute(*cmd[1:])
